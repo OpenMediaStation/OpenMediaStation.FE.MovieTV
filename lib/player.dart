8657b41c5348a2e6a4b7fbe5c3a689fa3eda20ac
Lukas Nagel <lukas@lna-dev.net>
2024-10-12 17:25:52 +0200

    🐛 Fixed player

--- lib/player.dart
+++ lib/player.dart
@@ -12,9 +12,7 @@ class PlayerView extends StatefulWidget {
 }
 
 class _PlayerState extends State<PlayerView> {
-  // Create a [Player] to control playback.
   late final player = Player();
-  // Create a [VideoController] to handle video output from [Player].
   late final controller = VideoController(player);
 
   @override
@@ -22,7 +20,6 @@ class _PlayerState extends State<PlayerView> {
     MediaKit.ensureInitialized();
 
     super.initState();
-    // Play a [Media] or [Playlist].
     player.open(
       Media(
         widget.url,
@@ -31,13 +28,23 @@ class _PlayerState extends State<PlayerView> {
   }
 
   @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.width * 9.0 / 16.0,
-        // Use [Video] widget to display video output.
-        child: Video(controller: controller),
+        child: Video(
+          controller: controller,
+          controls: (state) {
+            return AdaptiveVideoControls(state);
+          },
+        ),
       ),
     );
   }
