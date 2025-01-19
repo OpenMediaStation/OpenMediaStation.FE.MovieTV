28a4204f619f7a96a5e06d239a5984ac34716d7c
Lukas Nagel <github@lna-dev.net>
2025-01-19 11:30:46 +0100

    ✨ Added favorite indicator

--- lib/widgets/grid_item.dart
+++ lib/widgets/grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:open_media_server_app/globals/globals.dart';
 import 'package:open_media_server_app/models/internal/grid_item_model.dart';
 import 'package:open_media_server_app/widgets/custom_image.dart';
@@ -6,31 +7,49 @@ import 'package:open_media_server_app/widgets/custom_image.dart';
 class GridItem extends StatelessWidget {
   final GridItemModel item;
 
-  const GridItem({Key? key, required this.item, required this.desiredItemWidth}) : super(key: key);
+  const GridItem({
+    Key? key,
+    required this.item,
+    required this.desiredItemWidth,
+  }) : super(key: key);
 
   final double desiredItemWidth;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Stack(
       children: [
-        Expanded(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: CustomImage(
-              imageUrl: item.posterUrl ?? Globals.PictureNotFoundUrl,
-              fit: BoxFit.cover,
-              width: desiredItemWidth + 150,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: CustomImage(
+                  imageUrl: item.posterUrl ?? Globals.PictureNotFoundUrl,
+                  fit: BoxFit.cover,
+                  width: desiredItemWidth + 150,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              item.inventoryItem?.title ?? "Unknown title",
+              style:
+                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        if (item.isFavorite ?? false) // Show only if `isFavorite` is true
+          const Positioned(
+            top: 8.0,
+            right: 8.0,
+            child: Icon(
+              AntDesign.heart_fill,
+              color: Colors.red,
             ),
           ),
-        ),
-        const SizedBox(height: 8.0),
-        Text(
-          item.inventoryItem?.title ?? "Unknown title",
-          style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
       ],
     );
   }
