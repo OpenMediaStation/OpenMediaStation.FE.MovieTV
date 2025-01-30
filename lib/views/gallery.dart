30c758f2bf84b2d32cdd6c934294f540d35be355
redinsch <redinsch@gmx.de>
2025-01-30 23:26:07 +0100

    🐛 💄 fixed some bugs and moved the searchbar into the appbar

--- lib/views/gallery.dart
+++ lib/views/gallery.dart
@@ -29,9 +29,11 @@ class _GalleryState extends State<Gallery> {
   bool _descending = false;
   bool displayMovies = true;
   bool displayShows = true;
+  bool searchbarVisible = false;
   GlobalKey fstItemGlobalKey = GlobalKey();
   double? gridItemHeight;
   ValueNotifier<bool> filterChanged = ValueNotifier<bool>(false);
+  late Future<List<InventoryItem>> futureItems;
 
   void _getItemSize() {
     if (fstItemGlobalKey.currentContext != null) {
@@ -42,39 +44,91 @@ class _GalleryState extends State<Gallery> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    var futureItems = getInventoryItems();
+  void initState() {
+    futureItems = getInventoryItems();
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double desiredItemWidth = 150;
+    bool largeScreen = false;
     if (screenWidth > 1000) {
       desiredItemWidth = 300;
+      largeScreen = true;
     }
     int crossAxisCount = (screenWidth / desiredItemWidth).floor();
     double gridItemAspectRatio = 0.6;
 
+    var searchBar = Flexible(
+      fit: FlexFit.tight,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 550, minWidth: 200),
+        child: Padding(
+            padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+            child: TextField(
+              controller: searchController,
+              expands: false,
+              decoration: const InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {});
+              },
+            )),
+      ),
+    );
+    var title = Flexible(
+      child: FittedBox(
+        child: Row(
+          children: [
+            const Image(
+              image: AssetImage('assets/AppImage/myapp.png'),
+              height: 60,
+            ),
+            Text(screenWidth > 300 ? Globals.Title : ''),
+          ],
+        ),
+      ),
+    );
+
+    List<Widget> appBarTitleSpace = [];
+    if ((PlatformGlobals.isMobile || !largeScreen) && searchbarVisible) {
+      appBarTitleSpace.add(searchBar);
+    } else if (searchbarVisible) {
+      appBarTitleSpace.addAll([title, searchBar]);
+    } else {
+      appBarTitleSpace.add(title);
+    }
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: !largeScreen,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         surfaceTintColor: Colors.transparent,
         title: Row(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Image(
-                image: AssetImage('assets/AppImage/myapp.png'),
-                height: 60,
-              ),
-            ),
-            Text(Globals.Title),
-          ],
+          mainAxisAlignment: largeScreen
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          children: appBarTitleSpace,
         ),
         automaticallyImplyLeading: false,
         actions: [
-          PlatformGlobals.isKiosk
-              ? IconButton(
-                  onPressed: () => exit(0), icon: const Icon(Icons.close))
-              : const Text(''),
+          IconButton(
+              onPressed: () => setState(() {
+                    searchbarVisible = !searchbarVisible;
+                    if (!searchbarVisible) {
+                      searchController.text = "";
+                    }
+                  }),
+              icon: !searchbarVisible
+                  ? const Icon(Icons.search)
+                  : const Icon(Icons.search_off)),
           IconButton(
               onPressed: () => setState(() {
                     _descending = !_descending;
@@ -138,63 +192,50 @@ class _GalleryState extends State<Gallery> {
                       },
                     )
                   },
-              icon: const Icon(Icons.filter_alt))
+              icon: const Icon(Icons.filter_alt)),
+          PlatformGlobals.isKiosk
+              ? IconButton(
+                  onPressed: () => exit(0), icon: const Icon(Icons.close))
+              : const Text(''),
+          PlatformGlobals.isMobile
+              ? const Text('')
+              : IconButton(
+                  onPressed: () => setState(() {
+                        futureItems = getInventoryItems();
+                      }),
+                  icon: const Icon(Icons.refresh))
         ],
       ),
       body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              PreferredSize(
-                  preferredSize: const Size(0, 20),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        right: 8.0, left: 8.0, bottom: 15),
-                    child: TextField(
-                      controller: searchController,
-                      expands: false,
-                      decoration:
-                          const InputDecoration(icon: Icon(Icons.search)),
-                      onChanged: (value) {
-                        setState(() {});
-                      },
-                    ),
-                  )),
-              FutureBuilder<List<InventoryItem>>(
-                  future: futureItems,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return const Center(child: Text('No movies found.'));
-                    }
+          child: FutureBuilder<List<InventoryItem>>(
+              future: futureItems,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No movies found.'));
+                }
 
-                    List<InventoryItem> items = snapshot.data!;
-                    List<InventoryItem> filteredItems = items
-                        .where((item) =>
-                            (searchController.text == '' ||
-                                (item.title?.toLowerCase().contains(
-                                        searchController.text.toLowerCase()) ??
-                                    false)) &&
-                            ((item.category == "Movie" && displayMovies) ||
-                                (item.category == "Show" && displayShows)))
-                        .toList();
+                List<InventoryItem> items = snapshot.data!;
+                List<InventoryItem> filteredItems = filterItems(items);
 
-                    if (_descending) {
-                      filteredItems.sort((a, b) =>
-                          b.title
-                              ?.toLowerCase()
-                              .compareTo(a.title?.toLowerCase() ?? '') ??
-                          0);
-                    }
-
-                    return Expanded(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Expanded(
+                return Stack(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          width: screenWidth - 50,
+                          child: RefreshIndicator(
+                            displacement: 40,
+                            onRefresh: () async {
+                              setState(() {
+                                futureItems = getInventoryItems();
+                              });
+                            },
                             child: GridView.builder(
                               controller: _scrollController,
                               itemCount: filteredItems.length,
@@ -208,13 +249,13 @@ class _GalleryState extends State<Gallery> {
                               ),
                               itemBuilder: (context, index) {
                                 return FutureBuilder<GridItemModel>(
-                                  future:
-                                      filteredItems[index].category == "Movie"
-                                          ? getMovie(filteredItems[index])
-                                          : getShow(filteredItems[index]),
+                                  future: filteredItems[index].category ==
+                                          "Movie"
+                                      ? getMovie(filteredItems[index])
+                                      : getShow(filteredItems[index]),
                                   builder: (context, snapshot) {
                                     GridItemModel gridItem;
-
+                                          
                                     if (snapshot.connectionState ==
                                         ConnectionState.waiting) {
                                       gridItem = GridItemModel(
@@ -222,14 +263,14 @@ class _GalleryState extends State<Gallery> {
                                         metadataModel: null,
                                         isFavorite: null,
                                       );
-
+                                          
                                       gridItem.fake = true;
                                       gridItem.posterUrl =
                                           "${Preferences.prefs?.getString("BaseUrl")}/images/${filteredItems[index].category}/${filteredItems[index].metadataId}/poster";
                                     } else if (snapshot.hasError) {
                                       return Center(
-                                          child:
-                                              Text('Error: ${snapshot.error}'));
+                                          child: Text(
+                                              'Error: ${snapshot.error}'));
                                     } else if (!snapshot.hasData) {
                                       return const Center(
                                           child: Text(
@@ -237,7 +278,7 @@ class _GalleryState extends State<Gallery> {
                                     } else {
                                       gridItem = snapshot.data!;
                                     }
-
+                                          
                                     Key? inkwellKey;
                                     if ((fstItemGlobalKey.currentContext ==
                                         null)) {
@@ -245,7 +286,7 @@ class _GalleryState extends State<Gallery> {
                                     } else if (gridItemHeight == null) {
                                       _getItemSize();
                                     }
-
+                                          
                                     return InkWell(
                                       key: inkwellKey,
                                       child: GridItem(
@@ -272,7 +313,7 @@ class _GalleryState extends State<Gallery> {
                                                   itemModel: gridItem,
                                                 );
                                               }
-
+                                          
                                               throw ArgumentError(
                                                   "Server models not correct");
                                             }),
@@ -285,50 +326,75 @@ class _GalleryState extends State<Gallery> {
                               },
                             ),
                           ),
-                          SizedBox(
-                            width: 40,
-                            child: AlphabetScrollbar(
-                              letterCollection: filteredItems
-                                  .map((item) =>
-                                      item.title
-                                          ?.toUpperCase()
-                                          .characters
-                                          .first ??
-                                      "")
-                                  .where((c) => c != "")
-                                  .toSet()
-                                  .toList(),
-                              onLetterChange: (letter) {
-                                // await
-                                HapticFeedback.mediumImpact();
-                                _scrollController
-                                  .animateTo(
-                                  ((gridItemHeight ??
-                                              (desiredItemWidth /
-                                                  gridItemAspectRatio)) +
-                                          8) *
-                                      (filteredItems.indexWhere((item) =>
-                                              item.title
-                                                  ?.toUpperCase()
-                                                  .startsWith(letter) ??
-                                              false) ~/
-                                          crossAxisCount),
-                                  duration: const Duration(milliseconds: 500),
-                                  curve: Curves.fastOutSlowIn,
-                                );
-                              },
-                              factor: 15,
-                              selectedLetterAdditionalSpace: 10,
-                              reverse: _descending,
-                            ),
-                          )
-                        ],
+                        ),
+                        const Expanded(child: Text(''))
+                      ],
+                    ),
+                    if (filteredItems.length ~/ crossAxisCount > 5)
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      top: 0,
+                      child: AlphabetScrollbar(
+                        letterCollection: createAlphabetList(filteredItems),
+                        onLetterChange: (letter) {
+                          // await
+                          HapticFeedback.mediumImpact();
+                          _scrollController.animateTo(
+                            ((gridItemHeight ??
+                                        (desiredItemWidth /
+                                            gridItemAspectRatio)) +
+                                    8) *
+                                (filteredItems.indexWhere((item) =>
+                                        item.title
+                                            ?.toUpperCase()
+                                            .startsWith(letter) ??
+                                        false) ~/
+                                    crossAxisCount),
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.fastOutSlowIn,
+                          );
+                        },
+                        factor: 50,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        selectedLetterAdditionalSpace: 25,
+                        selectedLetterColor: Colors.black,
+                        reverse: _descending,
+                        selectedLetterContainerDecoration:
+                            const BoxDecoration(
+                                color: Color.fromARGB(255, 194, 194, 194),
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                              BoxShadow(color: Colors.black, blurRadius: 7)
+                            ]),
+                        selectedLettercontainerPadding:
+                            const EdgeInsets.all(2),
+                        letterContainerDecoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(179, 41, 41, 41),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black,
+                                  blurStyle: BlurStyle.outer,
+                                  blurRadius: 7)
+                            ]),
+                        letterContainerPadding: const EdgeInsets.all(2),
                       ),
-                    );
-                  })
-            ],
-          )),
+                    ),
+                  ],
+                );
+              })),
     );
+  }
+
+  List<String> createAlphabetList(List<InventoryItem> filteredItems) {
+    var letters = filteredItems
+        .map((item) => item.title?.toUpperCase().characters.first ?? "")
+        .where((c) => c != "")
+        .toSet()
+        .toList();
+    letters.sort((n, m) => n.compareTo(m));
+    return letters;
   }
 
   Future<List<InventoryItem>> getInventoryItems() async {
@@ -393,5 +459,32 @@ class _GalleryState extends State<Gallery> {
     gridItem.backdropUrl = metadata?.show?.backdrop;
     gridItem.childIds = show.seasonIds;
     return gridItem;
+  }
+  
+  List<InventoryItem> filterItems(List<InventoryItem> items) {
+    var filteredList = items
+                    .where((item) =>
+                        (searchController.text == "" ||
+                            (item.title?.toLowerCase().contains(
+                                    searchController.text.toLowerCase()) ??
+                                false)) &&
+                        ((item.category == "Movie" && displayMovies) ||
+                            (item.category == "Show" && displayShows)))
+                    .toList();
+
+                filteredList.sort((a, b) =>
+                    a.title
+                        ?.toLowerCase()
+                        .compareTo(b.title?.toLowerCase() ?? '') ??
+                    0);
+
+                if (_descending) {
+                  filteredList.sort((a, b) =>
+                      b.title
+                          ?.toLowerCase()
+                          .compareTo(a.title?.toLowerCase() ?? '') ??
+                      0);
+                }
+                return filteredList;
   }
 }
