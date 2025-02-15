191776d21a4274e2e6172dec19ccf31d0725462b
Lukas Nagel <github@lna-dev.net>
2025-02-15 14:29:05 +0100

    ✨ Added indicators to season

--- lib/views/detail_views/show_detail.dart
+++ lib/views/detail_views/show_detail.dart
@@ -1,6 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:open_media_server_app/apis/base_api.dart';
 import 'package:open_media_server_app/apis/favorites_api.dart';
 import 'package:open_media_server_app/apis/inventory_api.dart';
 import 'package:open_media_server_app/apis/metadata_api.dart';
@@ -8,9 +6,9 @@ import 'package:open_media_server_app/apis/progress_api.dart';
 import 'package:open_media_server_app/globals/globals.dart';
 import 'package:open_media_server_app/models/internal/grid_item_model.dart';
 import 'package:open_media_server_app/models/metadata/metadata_model.dart';
-import 'package:open_media_server_app/views/detail_views/season_detail.dart';
 import 'package:open_media_server_app/widgets/custom_image.dart';
 import 'package:open_media_server_app/widgets/favorite_button.dart';
+import 'package:open_media_server_app/widgets/season_card.dart';
 import 'package:open_media_server_app/widgets/title.dart';
 
 class ShowDetailView extends StatelessWidget {
@@ -46,57 +44,8 @@ class ShowDetailView extends StatelessWidget {
           List<Widget> seasons = [];
 
           for (var element in items) {
-            String imageUrl = Globals.PictureNotFoundUrl;
-
-            if (element.posterUrl != null) {
-              imageUrl = "${element.posterUrl!}?height=300";
-            }
-
             seasons.add(
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                    splashColor: Colors.black26,
-                    child: Column(
-                      children: [
-                        Ink.image(
-                          height: 300,
-                          width: 300 * (9 / 14),
-                          fit: BoxFit.cover,
-                          image: CachedNetworkImageProvider(
-                            imageUrl,
-                            errorListener: (p0) {
-                              print("fuck");
-                            },
-                            headers: BaseApi.getHeaders(),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          "${element.inventoryItem?.title}",
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              SeasonDetailView(itemModel: element),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
+              SeasonCard(element: element),
             );
           }
 
